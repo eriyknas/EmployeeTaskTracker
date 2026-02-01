4630d51802387e7857e2f1df025b056819fe3478
eriyknas <anastasia9cheshuina@yandex.ru>
2026-02-01 19:38:54 +0300

    Demonstrate polymorphism with ReportGenerator list

--- src/ReportGenerator.kt
+++ src/ReportGenerator.kt
@@ -1,1 +1,29 @@
-interface ReportGenerator{}
+
+interface ReportGenerator {
+    fun generateReport(): String
+}
+class EmployeeWithReport(
+    val name: String,
+    val position: String
+) : ReportGenerator {
+
+    override fun generateReport(): String {
+        return "Отчёт по сотруднику:\nИмя: $name\nДолжность: $position"
+    }
+}
+class DevelopmentDepartmentWithReport : ReportGenerator {
+    val name = "Отдел разработки"
+
+    override fun generateReport(): String {
+        return "Отчёт по отделу:\nНазвание: $name\nЗадача: Разработка ПО"
+    }
+}
+fun main() {
+    val reports: List<ReportGenerator> = listOf(
+        EmployeeWithReport("Анна Сидорова", "Дизайнер"),
+        DevelopmentDepartmentWithReport()
+    )
+    for (report in reports) {
+        println(report.generateReport())
+    }
+}
